69ce4051b07dc89d9fcb6f5c62406ad44fd627b1
Nox <the0neWhoKnocks@users.noreply.github.com>
2020-09-08 19:49:30 -0700

    Updated JS hooks

--- hooks/js/bin/setup-hooks.sh
+++ hooks/js/bin/setup-hooks.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
-IN_CI=$(printenv | sed 's;=.*;;' | grep -E "(CI|JENKINS)")
+CI_VARS=$(printenv | sed 's;=.*;;' | grep -E "(CI|JENKINS)")
 
-if [ ! $IN_CI ]; then
+if [[ "$CI_VARS" == "" ]]; then
   SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
   REPO_ROOT="$SCRIPT_DIR/.."
 
-  if [ ! -d "$REPO_ROOT/.git/hooks.bak" ]; then
-    mv "$REPO_ROOT/.git/hooks" "$REPO_ROOT/.git/hooks.bak"
+  # Create symlink if one doesn't already exist
+  if [ ! -L "$REPO_ROOT/.git/hooks" ]; then
+    echo;
+    
+    # Back up the current hooks folder just in case a User has something in
+    # there they want to keep.
+    if [ -d "$REPO_ROOT/.git/hooks" ]; then
+      echo "[BACK-UP] 'hooks' folder to 'hooks.bak'"
+      mv "$REPO_ROOT/.git/hooks" "$REPO_ROOT/.git/hooks.bak"
+    fi
+
+    # Create the symlink
     ln -s "$REPO_ROOT/.githooks/" "$REPO_ROOT/.git/hooks"
+    echo "[CREATED] Sym-link for hooks"
+  else
+    echo;
+    echo "[SKIP] Sym-link creation, hooks are already wired up"
   fi
+else
+  echo;
+  echo "[DETECTED] CI environment, sym-link won't be created for hooks"
+  echo "$CI_VARS" | while IFS= read -r var ; do echo "  - $var"; done
 fi
